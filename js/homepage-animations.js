21dba705f49f9a208f7bdff24394a365d29f205e
Arshad <arshad.aboo@standardbank.co.za>
2019-10-09 21:08:10 +0200

    fix console errors, add license

--- js/homepage-animations.js
+++ js/homepage-animations.js
@@ -1,25 +1,25 @@
 /* Homepage */
-var food = document.querySelector('#food');
-var desserts = document.querySelector('#desserts');
-var drinks = document.querySelector('#drinks');
-var aboutDescription  = document.querySelector('#about-description');
-var card1 = document.querySelector('#card-1');
-var card2 = document.querySelector('#card-2');
-var card3 = document.querySelector('#card-3');
-var newsletterForm = document.querySelector('#newsletter-form');
-food.style.opacity = "0";
-desserts.style.opacity = "0";
-drinks.style.opacity = "0";
+var food = document.querySelector("#food");
+var desserts = document.querySelector("#desserts");
+var drinks = document.querySelector("#drinks");
+var aboutDescription = document.querySelector("#about-description");
+var card1 = document.querySelector("#card-1");
+var card2 = document.querySelector("#card-2");
+var card3 = document.querySelector("#card-3");
+var newsletterForm = document.querySelector("#newsletter-form");
+// food.style.opacity = "0";
+// desserts.style.opacity = "0";
+// drinks.style.opacity = "0";
 aboutDescription.style.opacity = "0";
-card1.style.opacity = "0";
-card2.style.opacity = "0";
-card3.style.opacity = "0";
-newsletterForm.style.opacity = "0";
+// card1.style.opacity = "0";
+// card2.style.opacity = "0";
+// card3.style.opacity = "0";
+// newsletterForm.style.opacity = "0";
 
 var options = {
-  rootMargin: '0px',
+  rootMargin: "0px",
   threshold: 0.2
-}
+};
 
 function callback(entries, observer) {
   entries.forEach(function(entry) {
@@ -95,12 +95,12 @@ function callback(entries, observer) {
 // Create an intersection observer
 var observer = new IntersectionObserver(callback, options);
 
-// Start observing 
-observer.observe(food);
-observer.observe(desserts);
-observer.observe(drinks);
+// Start observing
+// observer.observe(food);
+// observer.observe(desserts);
+// observer.observe(drinks);
 observer.observe(aboutDescription);
-observer.observe(card1);
-observer.observe(card2);
-observer.observe(card3);
-observer.observe(newsletterForm);
+// observer.observe(card1);
+// observer.observe(card2);
+// observer.observe(card3);
+// observer.observe(newsletterForm);
